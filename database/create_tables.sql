76f492afffd5cbc818b4181a0c21d742bfea56a2
Susanna Hyötyläinen <a1402957@myy.haaga-helia.fi>
2016-04-06 20:08:41 +0300

    tietokantalausteet lisätty

---
+++ database/create_tables.sql
@@ -0,0 +1,22 @@
+CREATE TABLE kayttaja (
+	id INT NOT NULL AUTO_INCREMENT,
+	etunimi VARCHAR(20) NOT NULL,
+	sukunimi VARCHAR(30) NOT NULL,
+	sahkoposti VARCHAR(25) NOT NULL,
+	kuvaus VARCHAR(100),
+	PRIMARY KEY (id)
+);
+ 
+CREATE TABLE kiinnostus (
+	kiinnostus_id INT NOT NULL AUTO_INCREMENT,
+	nimi VARCHAR(30) NOT NULL, 
+	PRIMARY KEY(kiinnostus_id)
+);
+
+CREATE TABLE kayttajanKiinnostus (
+	kayttaja_id INT NOT NULL,
+	kiinnostus_id INT NOT NULL,
+	PRIMARY KEY(kayttaja_id, kiinnostus_id),
+	FOREIGN KEY(kayttaja_id) REFERENCES kayttaja (id),
+	FOREIGN KEY(kiinnostus_id) REFERENCES kiinnostus (kiinnostus_id)
+);
