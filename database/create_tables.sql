93e5b1325446d1364fe805ec0e292797b8cb901b
Susanna Hyötyläinen <a1402957@myy.haaga-helia.fi>
2016-05-03 10:09:28 +0300

    lisätty utf8 asetukset tietokantaan ja projektiin

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -5,13 +5,13 @@ CREATE TABLE kayttaja (
 	sahkoposti VARCHAR(25) NOT NULL,
 	kuvaus VARCHAR(100),
 	PRIMARY KEY (id)
-);
+)ENGINE=InnoDB CHARACTER SET=UTF8;
  
 CREATE TABLE kiinnostus (
 	kiinnostus_id INT NOT NULL AUTO_INCREMENT,
 	nimi VARCHAR(30) NOT NULL, 
 	PRIMARY KEY(kiinnostus_id)
-);
+)ENGINE=InnoDB CHARACTER SET=UTF8;
 
 CREATE TABLE kayttajanKiinnostus (
 	kayttaja_id INT NOT NULL,
@@ -19,4 +19,4 @@ CREATE TABLE kayttajanKiinnostus (
 	PRIMARY KEY(kayttaja_id, kiinnostus_id),
 	FOREIGN KEY(kayttaja_id) REFERENCES kayttaja (id),
 	FOREIGN KEY(kiinnostus_id) REFERENCES kiinnostus (kiinnostus_id)
-);
+)ENGINE=InnoDB CHARACTER SET=UTF8;
